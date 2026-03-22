d587c07c782dbfb805926778ed4d84007ef235ca
hackman-linux <ndjodongouhs@gmail.com>
2026-03-22 07:50:55 +0100

    frontent almost done

--- napster/urls.py
+++ napster/urls.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.music.urls')),
-    path('accounts/', include('apps.accounts.urls')),
-    path('search/', include('apps.search.urls')),
+    path('admin/',     admin.site.urls),
+    path('',           include('apps.music.urls')),
+    path('accounts/',  include('apps.accounts.urls')),
+    path('search/',    include('apps.search.urls')),
     path('playlists/', include('apps.playlists.urls')),
-    path('social/', include('apps.social.urls')),
-    path('api/', include('apps.api.urls')),
+    path('social/',    include('apps.social.urls')),
+    path('api/',       include('apps.api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
